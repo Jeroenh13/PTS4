0672865f4838d5b15db12d949fad9c6df565ec3e
kitty <kitty@DESKTOP-99IHB59>
2015-09-23 11:47:43 +0200

    Begin unit beheren

---
+++ CIMS/src/cims/Unit.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cims;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author kitty
+ */
+public class Unit implements Serializable{
+    
+    private String name = "";
+    private String function = "";
+    private String available = "";
+    private String department = "";
+    private String town = "";
+    private int level = 0;
+    private String team = "";
+    private String appointedTo = "";
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the function
+     */
+    public String getFunction() {
+        return function;
+    }
+
+    /**
+     * @param function the function to set
+     */
+    public void setFunction(String function) {
+        this.function = function;
+    }
+
+    /**
+     * @return the available
+     */
+    public String getAvailable() {
+        return available;
+    }
+
+    /**
+     * @param available the available to set
+     */
+    public void setAvailable(String available) {
+        this.available = available;
+    }
+
+    /**
+     * @return the department
+     */
+    public String getDepartment() {
+        return department;
+    }
+
+    /**
+     * @param department the department to set
+     */
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    /**
+     * @return the town
+     */
+    public String getTown() {
+        return town;
+    }
+
+    /**
+     * @param town the town to set
+     */
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    /**
+     * @return the level
+     */
+    public int getLevel() {
+        return level;
+    }
+
+    /**
+     * @param level the level to set
+     */
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    /**
+     * @return the team
+     */
+    public String getTeam() {
+        return team;
+    }
+
+    /**
+     * @param team the team to set
+     */
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    /**
+     * @return the appointedTo
+     */
+    public String getAppointedTo() {
+        return appointedTo;
+    }
+
+    /**
+     * @param appointedTo the appointedTo to set
+     */
+    public void setAppointedTo(String appointedTo) {
+        this.appointedTo = appointedTo;
+    }
+    
+    public void Sort(String searchOn, String value)
+    {
+        
+    }
+}
