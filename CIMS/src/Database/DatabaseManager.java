8b31c163467a36c0cf9c5189941d45a5075abc45
94BasMulder <bassie780@gmail.com>
2015-09-23 10:57:11 +0200

    Database Manager
    Database Manager toegevoegt aan het project.

---
+++ CIMS/src/Database/DatabaseManager.java
@@ -0,0 +1,83 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * *
+ *
+ * Class used for a connection to communicate with the database
+ */
+public class DatabaseManager {
+
+    private Connection conn = null;
+
+    /**
+     * *
+     * Opens the database connection
+     *
+     * @return if the opening of the connection is succesfull
+     */
+    private boolean openConnection() {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://athena01.fhict.local/dbi298273?user=dbi298273&password=AbEc65A52w");
+            return true;
+        } catch (SQLException e) {
+            conn = null;
+            System.out.println(e.getMessage());
+            System.out.println(e.getSQLState());
+            return false;
+        }
+    }
+    
+    /***
+     * Closes the connection
+     * @return succesrate 
+     */
+    private boolean closeConnection(){
+        try{
+        conn.close();
+        return true;
+        }catch(Exception e){return false;}
+    }
+
+    /**
+     * *
+     * Tests the connection to the database
+     *
+     * @return succes rate
+     * @throws java.sql.SQLException closing the connections
+     */
+    public boolean testConnection() throws SQLException {
+        if (!openConnection()) {
+            return false;
+        }
+        ResultSet result = null;
+        Statement statement;
+        boolean returnval = false;
+        try {
+            statement = conn.createStatement();
+            result = statement.executeQuery("Select * from Personeel");
+            statement.closeOnCompletion();
+            result.last();
+            if (result.getRow() != 0) {
+                returnval = true;
+            }
+            return returnval;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getSQLState());
+            conn = null;
+            return false;
+        } finally {
+            if (result != null) {
+                result.close();
+            }
+            closeConnection();
+        }
+    }
+
+}
