67398a5082dc24457a9904fcbf4cdb88fa59b824
94BasMulder <bassie780@gmail.com>
2016-01-06 10:51:23 +0100

    platforms do run later

--- CIMS/src/controller/CentralControllerFX.java
+++ CIMS/src/controller/CentralControllerFX.java
@@ -9,6 +9,7 @@ import ChatServer.ChatClient;
 import Server.ClientReceiving;
 import cims.Helpline;
 import cims.Report;
+import com.sun.jndi.dns.DnsContextFactory;
 import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -30,9 +32,8 @@ import javafx.util.Callback;
  *
  * @author kitty
  */
-public class CentralControllerFX extends controller.CentralController implements Initializable, Observer
-{
-    
+public class CentralControllerFX extends controller.CentralController implements Initializable, Observer {
+
     @FXML
     private TextField tfApproachPolice;
     @FXML
@@ -41,11 +42,11 @@ public class CentralControllerFX extends controller.CentralController implements
     private TextField tfApproachFirefighters;
     @FXML
     private Button btnSaveFirefighters;
-    @FXML 
+    @FXML
     private TextField tfApproachAmbulance;
     @FXML
     private Button btnSaveAmbulance;
-    
+
     @FXML
     private TableView tvVehAssPolice;
     @FXML
@@ -58,22 +59,22 @@ public class CentralControllerFX extends controller.CentralController implements
     private TableView tvVehAssAmbulance;
     @FXML
     private TableView tvVehAllAmbulance;
-    @FXML 
+    @FXML
     private TableView<Report> tvIncidents;
-    @FXML private Button btnInformationIncident;
-    
+    @FXML
+    private Button btnInformationIncident;
+
     @FXML
     private TextArea taChat;
     @FXML
     private TextField tfChatMessage;
-    
+
     int tmpID = 0;
-    
+
     private ChatClient cc = new ChatClient(1);
 
     private Thread chat;
-    
-    
+
     private ClientReceiving cr = new ClientReceiving();
     private Thread reportListener;
 
@@ -81,66 +82,57 @@ public class CentralControllerFX extends controller.CentralController implements
     public void initialize(URL url, ResourceBundle rb) {
         makeCollums();
         fillColums();
-        
+
         chat = new Thread(cc);
         chat.start();
         cc.addObserver(this);
-        
-        
+
         reportListener = new Thread(cr);
         reportListener.start();
         cr.addObserver(this);
     }
-    
-    public void savePolice(Event evnt)
-    {
+
+    public void savePolice(Event evnt) {
         System.out.println("doe iets");
         String plan = tfApproachPolice.getText();
         String helpline = "police";
         tfApproachPolice.clear();
         savePlan(plan, helpline, tmpID);
     }
-    
-    public void saveFirefighters(Event evnt)
-    {
+
+    public void saveFirefighters(Event evnt) {
         System.out.println("doe iets");
         String plan = tfApproachFirefighters.getText();
         String helpline = "firefighters";
         tfApproachFirefighters.clear();
         savePlan(plan, helpline, tmpID);
     }
-    
-    public void saveAmbulance(Event evnt)
-    {
+
+    public void saveAmbulance(Event evnt) {
         System.out.println("doe iets");
         String plan = tfApproachAmbulance.getText();
         String helpline = "ambulance";
         tfApproachAmbulance.clear();
         savePlan(plan, helpline, tmpID);
     }
-    
-    public void getAllVehicles()
-    {
-        
+
+    public void getAllVehicles() {
+
     }
-    
-    public void informationAccident()
-    {
-        
+
+    public void informationAccident() {
+
     }
-    
+
     public void btnSendChatClick(Event e) {
         cc.setText("Centrale: " + tfChatMessage.getText());
     }
-    
-    public void makeCollums()
-    {
+
+    public void makeCollums() {
         tvIncidents.getColumns().clear();
         List<Field> fields = getCollumsReport();
-        for (Field f : fields)
-        {
-            if (!f.getName().equals("dbm"))
-            {
+        for (Field f : fields) {
+            if (!f.getName().equals("dbm")) {
                 TableColumn tc = new TableColumn();
                 tc.setText(f.getName());
                 tc.setCellValueFactory(new PropertyValueFactory<>(f.getName()));
@@ -151,9 +143,8 @@ public class CentralControllerFX extends controller.CentralController implements
             }
         }
     }
-    
-    public void fillColums()
-    {
+
+    public void fillColums() {
         ObservableList<Report> reports = FXCollections.observableArrayList();
         reports = fillIncidents();
         tvIncidents.setItems(reports);
@@ -161,15 +152,19 @@ public class CentralControllerFX extends controller.CentralController implements
 
     @Override
     public void update(Observable o, Object arg) {
-        if(o.getClass() == ClientReceiving.class)
-        {
+        if (o.getClass() == ClientReceiving.class) {
+
             super.loadHelplines();
-            fillColums();
-            System.out.println("/yay");
-        }
-        else if(o.getClass() == ChatClient.class)
-        {
-            taChat.setText(taChat.getText() + "\n" + ((ChatClient)o).getText());
+            Platform.runLater(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    fillColums();
+                }
+            });
+        } else if (o.getClass() == ChatClient.class) {
+            taChat.setText(taChat.getText() + "\n" + ((ChatClient) o).getText());
         }
     }
 }
